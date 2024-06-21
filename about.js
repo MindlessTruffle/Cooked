9176ed9a6dd33f152cc8f8661b5c47f0df5a7449
MindlessTruffle <82486398+MindlessTruffle@users.noreply.github.com>
2024-06-21 14:20:28 +0000

    Mailto Subject Line

--- about.js
+++ about.js
@@ -1,6 +1,6 @@
 function Contact() {
     // window.location.href = 'mailto:hawas7818@wrdsb.ca';
-    window.open('mailto:hawas7818@wrdsb.ca', '_blank');
+    window.open('mailto:hawas7818@wrdsb.ca?subject=Question Or Sugggestion for "Am I Cooked" Site!', '_blank');
 }
 
 function goBack() {
