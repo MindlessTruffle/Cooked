cf73322ea218aeb27dde0764157667a8e301124f
MindlessTruffle <82486398+MindlessTruffle@users.noreply.github.com>
2024-06-21 14:13:19 +0000

    Added Redirect

---
+++ about.js
@@ -0,0 +1,8 @@
+function Contact() {
+    // window.location.href = 'mailto:hawas7818@wrdsb.ca';
+    window.open('mailto:hawas7818@wrdsb.ca', '_blank');
+}
+
+function goBack() {
+    window.location.href = 'index.html';
+}
